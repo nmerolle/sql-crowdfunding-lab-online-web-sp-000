fd766f65e16565cdf6217459996067d3043302ae
Nicholas Merolle <nmerolle11@hotmail.com>
2021-01-04 03:00:11 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -10,7 +10,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
   "SELECT projects.title, SUM(pledges.amount) FROM projects INNER JOIN pledges ON projects.id = pledges.project_id GROUP BY projects.id ORDER BY projects.title;"
 end
 
-def 
+def selects_the_user_name_age_and_pledge_amount_for_all_pledges
   "SELECT users.name, users.age, SUM(pledges.amount)
   FROM users
   INNER JOIN pledges
