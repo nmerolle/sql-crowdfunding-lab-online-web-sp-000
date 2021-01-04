b4ffeb0ee6519b69bda4a06cfba057d15181d22e
Nicholas Merolle <nmerolle11@hotmail.com>
2021-01-04 02:30:23 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -22,7 +22,7 @@ INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) V
 INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (2, 'My book on SQL', 'books', 20.00, '2013-01-01', '2014-01-01');
 INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (3, 'The next Harry Potter', 'books', 1000.00, '2013-05-30', '2015-05-30');
 INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (4, 'Animal shelter needs dog food', 'charity', 400.00, '2013-03-14', '2013-06-30');
-INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES 
 INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES
 INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES
 INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES
@@ -32,7 +32,7 @@ INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) V
 ,
 ,
 ,
-(5, 'Voldement needs a body', 'charity', 6000.00, '2013-03-20', '2013-09-20'),
+,
 (6, 'The next Inna-Gadda-Davida', 'music', 200.00, '2014-12-30', '2015-12-30'),
 (7, 'Iguana needs tail operation', 'charity', 2000.00, '2013-10-02', '2013-10-30'),
 (8, 'I have bed bugs!', 'charity', 800.00, '2014-06-30', '2014-06-31'),
