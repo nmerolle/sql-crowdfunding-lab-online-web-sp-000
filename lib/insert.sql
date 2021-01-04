90634d9c61d964d103ff13837e7687fc935cd21e
Nicholas Merolle <nmerolle11@hotmail.com>
2021-01-04 02:26:31 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -15,7 +15,7 @@ INSERT INTO users (id, name, age) VALUES (14, 'Sirius', 36)
 INSERT INTO users (id, name, age) VALUES (15, 'Albus', 113)
 INSERT INTO users (id, name, age) VALUES (16, 'Squid', 5)
 INSERT INTO users (id, name, age) VALUES (17, 'Whale', 6)
-INSERT INTO users (id, name, age) VALUES 
+INSERT INTO users (id, name, age) VALUES (18, 'Pacha', 5)
 INSERT INTO users (id, name, age) VALUES
 INSERT INTO users (id, name, age) VALUES, , , ,
 , , , , , ,
