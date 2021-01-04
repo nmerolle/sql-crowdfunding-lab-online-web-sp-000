bcbc96d8a13f3a0dd97459ccc803779c56b467d1
Nicholas Merolle <nmerolle11@hotmail.com>
2021-01-04 02:35:25 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -37,7 +37,7 @@ INSERT INTO pledges (id, amount, user_id, project_id) VALUES (6, 20.00, 4, 4);
 INSERT INTO pledges (id, amount, user_id, project_id) VALUES (7, 40.00, 5, 10);
 INSERT INTO pledges (id, amount, user_id, project_id) VALUES (8, 60.00, 6, 10);
 INSERT INTO pledges (id, amount, user_id, project_id) VALUES (9, 50.00, 7, 9);
-INSERT INTO pledges (id, amount, user_id, project_id) VALUES 
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES (10, 700.00, 8, 8)
 INSERT INTO pledges (id, amount, user_id, project_id) VALUES
 INSERT INTO pledges (id, amount, user_id, project_id) VALUES
 INSERT INTO pledges (id, amount, user_id, project_id) VALUES
