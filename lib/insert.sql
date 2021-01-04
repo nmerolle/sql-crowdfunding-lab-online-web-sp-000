2b899a076a23a129a8b3566094eb6b176499602c
Nicholas Merolle <nmerolle11@hotmail.com>
2021-01-04 02:29:50 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -29,7 +29,7 @@ INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) V
 INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES
 INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES
 ,
-(2, 'My book on SQL', 'books', 20.00, '2013-01-01', '2014-01-01'),
+,
 (3, 'The next Harry Potter', 'books', 1000.00, '2013-05-30', '2015-05-30'),
 (4, 'Animal shelter needs dog food', 'charity', 400.00, '2013-03-14', '2013-06-30'),
 (5, 'Voldement needs a body', 'charity', 6000.00, '2013-03-20', '2013-09-20'),
