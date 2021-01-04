4c1963f17095634f60126c8c2d1bc0bc22ac9cc6
Nicholas Merolle <nmerolle11@hotmail.com>
2021-01-04 02:24:33 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
 INSERT INTO users (id, name, age) VALUES (1, 'Finnebar', 17)
 INSERT INTO users (id, name, age) VALUES (2, 'Bear', 6)
 INSERT INTO users (id, name, age) VALUES (3, 'Iguana', 4)
+INSERT INTO users (id, name, age) VALUES (4, 'Alex', 33)
 INSERT INTO users (id, name, age) VALUES 
-INSERT INTO users (id, name, age) VALUES
 INSERT INTO users (id, name, age) VALUES
 INSERT INTO users (id, name, age) VALUES
 INSERT INTO users (id, name, age) VALUES
