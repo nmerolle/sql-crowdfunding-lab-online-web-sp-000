4ea9cdec40a74a80c8ebb9a267807caf2a5476c1
Nicholas Merolle <nmerolle11@hotmail.com>
2021-01-04 02:33:32 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -28,6 +28,7 @@ INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) V
 INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (8, 'I have bed bugs!', 'charity', 800.00, '2014-06-30', '2014-06-31');
 INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (9, 'I want to teach English in China', 'charity', 3000.00, '2013-06-30', '2013-09-30');
 INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (10, 'Help save birds of paradise', 'charity', 5000.00, '2012-03-20', '2013-06-30');
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES 
 INSERT INTO pledges (id, amount, user_id, project_id) VALUES
 INSERT INTO pledges (id, amount, user_id, project_id) VALUES
 INSERT INTO pledges (id, amount, user_id, project_id) VALUES
@@ -57,8 +58,7 @@ INSERT INTO pledges (id, amount, user_id, project_id) VALUES
 INSERT INTO pledges (id, amount, user_id, project_id) VALUES
 INSERT INTO pledges (id, amount, user_id, project_id) VALUES
 INSERT INTO pledges (id, amount, user_id, project_id) VALUES
-INSERT INTO pledges (id, amount, user_id, project_id) VALUES
-(1, 10.00, 1, 2), (2, 20.00, 1, 3), (3, 40.00, 1, 4), (4, 50.00, 2, 3),(5, 10.00, 3, 2),(6, 20.00, 4, 4),
+, (2, 20.00, 1, 3), (3, 40.00, 1, 4), (4, 50.00, 2, 3),(5, 10.00, 3, 2),(6, 20.00, 4, 4),
 (7, 40.00, 5, 10),(8, 60.00, 6, 10),(9, 50.00, 7, 9),(10, 700.00, 8, 8),(11, 1000.00, 8, 7),(12, 40.00, 9, 6),
 (13, 50.00, 9, 3),(14, 50.00, 10, 4),(15, 24.00, 12, 1),(16, 34.00, 11, 1),(17, 12.00, 13, 6),(18, 19.00, 14, 5),
 (19, 20.00, 15, 5),(20, 40.00, 16, 6),
