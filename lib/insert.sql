b8d5df36f19c315ba4aed1366613dc6ac1563ef5
Nicholas Merolle <nmerolle11@hotmail.com>
2021-01-04 02:30:44 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -34,7 +34,7 @@ INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) V
 ,
 ,
 ,
-(7, 'Iguana needs tail operation', 'charity', 2000.00, '2013-10-02', '2013-10-30'),
+,
 (8, 'I have bed bugs!', 'charity', 800.00, '2014-06-30', '2014-06-31'),
 (9, 'I want to teach English in China', 'charity', 3000.00, '2013-06-30', '2013-09-30'),
 (10, 'Help save birds of paradise', 'charity', 5000.00, '2012-03-20', '2013-06-30');
