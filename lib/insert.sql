0116b6dc63f6df3f055818555094dec28f97c951
Nicholas Merolle <nmerolle11@hotmail.com>
2021-01-04 02:34:03 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -30,8 +30,8 @@ INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) V
 INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (10, 'Help save birds of paradise', 'charity', 5000.00, '2012-03-20', '2013-06-30');
 INSERT INTO pledges (id, amount, user_id, project_id) VALUES (1, 10.00, 1, 2);
 INSERT INTO pledges (id, amount, user_id, project_id) VALUES (2, 20.00, 1, 3);
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES (3, 40.00, 1, 4);
 INSERT INTO pledges (id, amount, user_id, project_id) VALUES 
-INSERT INTO pledges (id, amount, user_id, project_id) VALUES
 INSERT INTO pledges (id, amount, user_id, project_id) VALUES
 INSERT INTO pledges (id, amount, user_id, project_id) VALUES
 INSERT INTO pledges (id, amount, user_id, project_id) VALUES
