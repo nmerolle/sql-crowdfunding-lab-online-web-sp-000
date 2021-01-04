6b30ca42a136d07361fa046c1fb1ee1391a6f696
Nicholas Merolle <nmerolle11@hotmail.com>
2021-01-04 02:34:43 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -33,6 +33,7 @@ INSERT INTO pledges (id, amount, user_id, project_id) VALUES (2, 20.00, 1, 3);
 INSERT INTO pledges (id, amount, user_id, project_id) VALUES (3, 40.00, 1, 4);
 INSERT INTO pledges (id, amount, user_id, project_id) VALUES (4, 50.00, 2, 3);
 INSERT INTO pledges (id, amount, user_id, project_id) VALUES (5, 10.00, 3, 2);
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES (6, 20.00, 4, 4);
 INSERT INTO pledges (id, amount, user_id, project_id) VALUES 
 INSERT INTO pledges (id, amount, user_id, project_id) VALUES
 INSERT INTO pledges (id, amount, user_id, project_id) VALUES
@@ -57,8 +58,6 @@ INSERT INTO pledges (id, amount, user_id, project_id) VALUES
 INSERT INTO pledges (id, amount, user_id, project_id) VALUES
 INSERT INTO pledges (id, amount, user_id, project_id) VALUES
 INSERT INTO pledges (id, amount, user_id, project_id) VALUES
-INSERT INTO pledges (id, amount, user_id, project_id) VALUES
-, , , ,,,
 (7, 40.00, 5, 10),(8, 60.00, 6, 10),(9, 50.00, 7, 9),(10, 700.00, 8, 8),(11, 1000.00, 8, 7),(12, 40.00, 9, 6),
 (13, 50.00, 9, 3),(14, 50.00, 10, 4),(15, 24.00, 12, 1),(16, 34.00, 11, 1),(17, 12.00, 13, 6),(18, 19.00, 14, 5),
 (19, 20.00, 15, 5),(20, 40.00, 16, 6),
