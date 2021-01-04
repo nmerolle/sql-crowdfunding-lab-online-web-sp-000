f73cd25a0ecd41e277eda6f02d9887b02f40b897
Nicholas Merolle <nmerolle11@hotmail.com>
2021-01-04 02:31:01 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -25,8 +25,8 @@ INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) V
 INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (5, 'Voldement needs a body', 'charity', 6000.00, '2013-03-20', '2013-09-20');
 INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (6, 'The next Inna-Gadda-Davida', 'music', 200.00, '2014-12-30', '2015-12-30');
 INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (7, 'Iguana needs tail operation', 'charity', 2000.00, '2013-10-02', '2013-10-30');
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (8, 'I have bed bugs!', 'charity', 800.00, '2014-06-30', '2014-06-31');
 INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES 
-INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES
 INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES
 ,
 ,
