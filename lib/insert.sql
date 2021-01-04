c20651cdface0782a2c71309c35f69a28c312e3c
Nicholas Merolle <nmerolle11@hotmail.com>
2021-01-04 02:29:47 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -18,8 +18,8 @@ INSERT INTO users (id, name, age) VALUES (17, 'Whale', 6);
 INSERT INTO users (id, name, age) VALUES (18, 'Pacha', 5);
 INSERT INTO users (id, name, age) VALUES (19, 'Ena', 24);
 INSERT INTO users (id, name, age) VALUES (20, 'Katie', 24);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (1, 'Help me buy a guitar', 'music', 500.00, '2013-06-30', '2013-07-30');
 INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES 
-INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES
 INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES
 INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES
 INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES
